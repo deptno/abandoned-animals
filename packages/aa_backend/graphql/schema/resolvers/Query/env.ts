072279a4ebcde804027c2c5a3b62aa2505538a23
bglee <deptno@gmail.com>
2020-01-12 04:59:34 +0900

    deploy

--- packages/aa_backend/graphql/schema/resolvers/Query/env.ts
+++ packages/aa_backend/graphql/schema/resolvers/Query/env.ts
@@ -1,9 +1,10 @@
-import {const_return_env_string} from '../../../../constant'
+import {const_return_env_string, const_stage} from '../../../../constant'
 import {QueryResolvers} from '@deptno/aa_graphql_type'
 
 export const env: QueryResolvers['env'] = (args, context) => {
   return JSON.stringify({
     claim: context.claim,
-    env_string: const_return_env_string
+    env_string: const_return_env_string,
+    stage: const_stage,
   })
 }
