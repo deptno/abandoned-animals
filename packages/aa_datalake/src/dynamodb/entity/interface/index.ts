c8062b227867a71ce234a0f3ae2c44d6f533ddd2
bglee <deptno@gmail.com>
2020-01-17 00:23:53 +0900

    feat: datalake 데이터 입력시 ttl 적용

--- packages/aa_datalake/src/dynamodb/entity/interface/index.ts
+++ packages/aa_datalake/src/dynamodb/entity/interface/index.ts
@@ -1,8 +1,10 @@
 import {AbandonedAnimal} from '@deptno/aa_parser'
+import {TTL} from '@deptno/dynamodb'
 
 export interface AADocument {
   hk: AbandonedAnimal['desertionNo']
   rk: AADocumentRkEnum
+  ttl: TTL
 }
 export enum AADocumentRkEnum {
   raw = 'raw'
--- packages/aa_datalake/src/dynamodb/entity/raw.ts
+++ packages/aa_datalake/src/dynamodb/entity/raw.ts
@@ -1,14 +1,18 @@
 import {AADocument, AADocumentRkEnum} from './interface'
 import {AbandonedAnimal} from '@deptno/aa_parser'
 import {Gzip, util} from '@deptno/dynamodb'
+import {addDays} from 'date-fns'
+import {const_ttl_days} from '../../constant'
 
 export class Raw implements AADocument {
   hk
   rk = AADocumentRkEnum.raw
+  ttl
   z: Gzip<AbandonedAnimal>
 
   constructor(data: AbandonedAnimal) {
     this.hk = data.desertionNo
+    this.ttl = util.ttl(addDays(Date.now(), const_ttl_days))
     this.z = util.gzip(data)
   }
 }
