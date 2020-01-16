6f75ef90c32240a19b2bebde8cc21232a555898f
bglee <deptno@gmail.com>
2020-01-17 00:23:53 +0900

    feat: datalake 데이터 입력시 ttl 적용

--- packages/aa_datalake/src/constant.ts
+++ packages/aa_datalake/src/constant.ts
@@ -2,6 +2,7 @@ import {defined} from '@deptno/aa_assert'
 
 export const const_open_api_key = process.env.AA_OPENAPI_KEY!
 export const const_aws_ddb_table = process.env.AA_DDB_TABLE!
+export const const_ttl_days = parseInt(process.env.AA_TTL_DAYS ?? '1')
 export const const_aws_region = process.env.AWS_REGION!
 
 defined(!const_open_api_key)
