173896d2abc5a01c3d55889e6f32c688f2976199
bglee <deptno@gmail.com>
2020-01-19 12:41:36 +0900

    test: fe deploy

--- packages/aa_frontend/next.config.js
+++ packages/aa_frontend/next.config.js
@@ -1,6 +1,9 @@
 const [_, assetPrefix] = (process.env.GITHUB_REPOSITORY || '').split('/')
-
-module.exports = {
+const config = {
   env: process.env.AA_GRAPHQL_URL || '/api/graphql',
   assetPrefix
 }
+
+console.log('build config:', config)
+
+module.exports = config
