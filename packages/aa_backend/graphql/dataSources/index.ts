bc30986470c34157a4fa4e8408e2574d4d10abff
deptno <deptno@gmail.com>
2020-01-20 21:02:21 +0900

    feat: 데이터소스에서 테이블 명을 인자로 받음 feat: 데이터를 GSI로 부터 프도록 변경

--- packages/aa_backend/graphql/dataSources/index.ts
+++ packages/aa_backend/graphql/dataSources/index.ts
@@ -1,9 +1,10 @@
-import {AADynamoDBDataSource} from '@deptno/aa_data_source'
 import {DynamoDB} from 'aws-sdk'
+import {const_aws_ddb_table} from '@deptno/aa_datalake/src/constant'
+import {AADynamoDBDataSource} from '@deptno/aa_data_source'
 
 export const dataSources = () => {
   return {
-    aa: new AADynamoDBDataSource(documentClient)
+    aa: new AADynamoDBDataSource(documentClient, const_aws_ddb_table)
   }
 }
 
--- packages/aa_data_source/src/AADynamoDBDataSource.ts
+++ packages/aa_data_source/src/AADynamoDBDataSource.ts
@@ -2,14 +2,16 @@ import {DataSource} from 'apollo-datasource'
 import {Species} from './type'
 import {createDynamoDB} from '@deptno/dynamodb'
 import {DocumentClient} from 'aws-sdk/clients/dynamodb'
-import {Raw} from './entity'
+import {AADocumentRkEnum, Raw} from './entity'
 
 export class AADynamoDBDataSource extends DataSource {
   #ddb: ReturnType<typeof createDynamoDB>
+  #tableName: string
 
-  constructor(documentClient: DocumentClient) {
+  constructor(documentClient: DocumentClient, tableName: string) {
     super()
     this.#ddb = createDynamoDB(documentClient)
+    this.#tableName = tableName
   }
 
   async getAbandonedAnimals(
@@ -23,11 +25,20 @@ export class AADynamoDBDataSource extends DataSource {
       limit?: number
     },
   ) {
+
     //todo: before, scanforward false
-    return this.#ddb.scan<Raw>({
-      TableName: 'dev-aa',
-      Limit: input.first,
+    return this.#ddb.query<Raw>({
+      TableName: this.#tableName,
+      IndexName: 'rkT',
+      KeyConditionExpression: '#h = :h',
+      ExpressionAttributeNames: {
+        '#h': 'rk',
+      },
+      ExpressionAttributeValues: {
+        ':h': AADocumentRkEnum.raw,
+      },
       ExclusiveStartKey: (input.after || input.before) as any,
+      Limit: input.first,
       ReturnConsumedCapacity: 'TOTAL'
     })
   }
