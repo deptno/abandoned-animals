3ea3cfc11937f00333e6f39dddb1f3dd9a738dac
deptno <deptno@gmail.com>
2020-01-21 11:04:40 +0900

    fix: 이틀에 대한 데이터를 가져와야하는데 하루만 가져오던 오류 수정

--- packages/aa_datalake/src/dynamodb/getRegisteredAnimals.ts
+++ packages/aa_datalake/src/dynamodb/getRegisteredAnimals.ts
@@ -1,12 +1,15 @@
 import {ddb} from '../lib/ddb'
 import {const_aws_ddb_table} from '../constant'
 import {AADocumentRkEnum, Raw} from '@deptno/aa_data_source/dist/entity'
-import {format, parse} from 'date-fns'
+import {addDays, format, parse} from 'date-fns'
 
 export const getRegisteredAnimals = ({startDate, endDate}: Input) => {
   const now = new Date()
   const startDateKST = parse(`${startDate}+0900`, 'yyyyMMddxx', now)
-  const endDateKST = parse(`${endDate}+0900`, 'yyyyMMddxx', now)
+  const endDateKST = addDays(
+    parse(`${endDate}+0900`, 'yyyyMMddxx', now),
+    1,
+  )
   const params = {
     TableName: const_aws_ddb_table,
     IndexName: 'rkT',
@@ -19,15 +22,15 @@ export const getRegisteredAnimals = ({startDate, endDate}: Input) => {
       ':h': AADocumentRkEnum.raw,
       ':s': Raw.key.t.stringify({
         rk: AADocumentRkEnum.raw,
-        createdAt: format(startDateKST, 'yyyy-MM-dd')
+        createdAt: format(startDateKST, 'yyyy-MM-dd'),
       }),
       ':e': Raw.key.t.stringify({
         rk: AADocumentRkEnum.raw,
-        createdAt: format(endDateKST, 'yyyy-MM-dd')
+        createdAt: format(endDateKST, 'yyyy-MM-dd'),
       }),
     },
     ProjectionExpression: 'hk',
-    ReturnConsumedCapacity: 'TOTAL'
+    ReturnConsumedCapacity: 'TOTAL',
   }
 
   console.log({params})
